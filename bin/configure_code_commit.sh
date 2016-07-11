6a26497dc09f91f2f218921924433477a5b6dd9a
Brandon Joyce <bjoyce@covermymeds.com>
2016-07-10 21:18:24 -0400

    Adding codecommit domain to known hosts

--- bin/configure_code_commit.sh
+++ bin/configure_code_commit.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 mkdir /app/.ssh
 
+# create public and private keys from env vars
 echo "$CODE_COMMIT_PUBLIC_KEY" > /app/.ssh/codecommit_rsa.pub
 echo "$CODE_COMMIT_PRIVATE_KEY" > /app/.ssh/codecommit_rsa
+
+# configure the user when using CodeCommit
 cat >> /app/.ssh/config << EOF
 Host git-codecommit.*.amazonaws.com
   User $CODE_COMMIT_SSH_KEY_ID
   IdentityFile /app/.ssh/codecommit_rsa
 EOF
+
+# add amazonaws to known hosts
+ssh-keygen -R git-codecommit.us-east-1.amazonaws.com
+ssh-keyscan -H git-codecommit.us-east-1.amazonaws.com >> /app/.ssh/known_hosts
